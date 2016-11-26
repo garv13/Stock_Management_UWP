4eba390bde1493a3418775a11d61e035f24e03f9
akshaygupta94 <akshaygupta1994@outlook.com>
2016-11-26 23:14:43 +0530

    page ui complete

--- Stock_Management_UWP/Search_Page.xaml.cs
+++ Stock_Management_UWP/Search_Page.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -22,14 +23,53 @@ namespace Stock_Management_UWP
     /// </summary>
     public sealed partial class Search_Page : Page
     {
+        private static IMobileServiceTable<ProductClass> Table = App.MobileService.GetTable<ProductClass>();
+        public static MobileServiceCollection<ProductClass, ProductClass> items;
+        public static MobileServiceCollection<String, String> items2;
+
         public Search_Page()
         {
             this.InitializeComponent();
+            Loaded += Search_Page_Loaded;
+        }
+
+        private async void Search_Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<string> lol = new List<string>();
+            items = await Table.ToCollectionAsync();
+            event1.ItemsSource = items;
+            items2 = await Table.Select(ProductClass => ProductClass.Material).ToCollectionAsync();
+            lol = items2.Distinct().ToList<string>();
+
+            lol.Add("All");
+            matBox.ItemsSource = lol;
+
+            items2 = await Table.Select(ProductClass => ProductClass.Color).ToCollectionAsync();
+            lol = items2.Distinct().ToList<string>();
+
+            lol.Add("All");
+            matBox2.ItemsSource = lol;
+
+
+
         }
 
         private void Grid_Tapped(object sender, TappedRoutedEventArgs e)
         {
 
+        }
+        private async void loadTable()
+        {
+            var Quality = comboBox.SelectedItem as ComboBoxItem;
+            string Qual = Quality.Content as string;
+            var material = matBox.SelectedItem as string;
+            var color = matBox2.SelectedItem as string;
+            items = await Table.Where(ProductClass => ProductClass.Name.Contains(Product_Name_Box.Text.ToUpper()) && 
+            ProductClass.Source.Contains(Product_Source_Box.Text.ToUpper()) && 
+            ProductClass.Color.Contains(Product_Name_Box.Text) &&
+            ProductClass.Name.Contains(Product_Name_Box.Text) &&
+            ProductClass.Name.Contains(Product_Name_Box.Text)  ).ToCollectionAsync();
+            event1.ItemsSource = items;
         }
     }
 }
